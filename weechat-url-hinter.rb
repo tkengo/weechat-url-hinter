3e03c1a6b525bd07dea7c6c8caf93f65c5d953eb
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-03-27 11:12:49 +0900

    Refactoring part2

--- weechat-url-hinter.rb
+++ weechat-url-hinter.rb
@@ -42,6 +42,9 @@ class UrlList
   end
 end
 
+#
+# Register url-hinter plugin to weechat and do initialization.
+#
 def weechat_init
   Weechat.register('weechat-url-hinter', 'Kengo Tateish', '0.1', 'MIT License', 'Open an url in the weechat buffer to type a hint', '', '')
   Weechat.hook_command('url_hinter', 'description', 'args', 'args_description', '', 'launch_url_hinter', '');
@@ -63,36 +66,26 @@ def my_signal_cb(data, signal, signal_type)
   return Weechat::WEECHAT_RC_OK
 end
 
-def launch_url_hinter(data, buffer, argv)
+def launch_url_hinter(data, buffer_pointer, argv)
   clear_hints
 
-  return Weechat::WEECHAT_RC_OK unless include_url?(buffer)
+  buffer = Buffer.new(buffer_pointer)
+  return Weechat::WEECHAT_RC_OK unless buffer.has_url?
 
-  own_lines = Weechat.hdata_pointer(Weechat.hdata_get('buffer'), buffer, 'own_lines')
-  line = Weechat.hdata_pointer(Weechat.hdata_get('lines'), own_lines, 'first_line')
-
-  continue_count = get_continue_count(buffer)
+  continue_count = buffer.own_lines.count - Window.current.chat_height
 
   index = 0
-  while true
-    data = Weechat.hdata_pointer(Weechat.hdata_get('line'), line, 'data')
-    message = Weechat.hdata_string(Weechat.hdata_get('line_data'), data, 'message')
-    displayed = Weechat.hdata_char(Weechat.hdata_get('line_data'), data, 'displayed').to_s == '1'
-    UrlList.push_message(data, message.dup)
-    new_message = Weechat.string_remove_color(message, '')
-    if new_message =~ /(https?:\/\/[^ \(\)\r\n]*)/
-      if index >= continue_count
-        url = $1
-        new_message = new_message.split(url)
-        new_message = "#{new_message[0].to_s + Weechat.color("yellow")}[#{UrlList.add($1).to_s}]#{Weechat.color("red") + url[3..-1].to_s + Weechat.color("blue") + new_message[1].to_s}"
+  buffer.own_lines.each do |line|
+    UrlList.push_message(line.data_pointer, line.message.dup)
+    new_message = Weechat.string_remove_color(line.message, '')
+    if line.has_url?  && index >= continue_count
+      line.urls.each do |url|
+        new_message.gsub!(url, "#{Weechat.color("yellow")}[#{UrlList.add(url).to_s}]#{Weechat.color("red") + url[3..-1].to_s + Weechat.color("blue")}")
       end
     end
-    new_message = "#{Weechat.color('blue')}#{new_message}#{Weechat.color('reset')}"
-    Weechat.hdata_update(Weechat.hdata_get('line_data'), data, { 'message' => new_message })
+    line.message = "#{Weechat.color('blue')}#{new_message}#{Weechat.color('reset')}"
 
-    index += 1 if displayed
-    line = Weechat.hdata_pointer(Weechat.hdata_get('line'), line, 'next_line')
-    break if line.to_s == ''
+    index += 1 if line.displayed?
   end
 
   UrlList.hook_pointer = Weechat.hook_signal('input_text_changed', 'my_signal_cb', "")
@@ -100,32 +93,47 @@ def launch_url_hinter(data, buffer, argv)
   Weechat::WEECHAT_RC_OK
 end
 
+#
+# Clear hints.
+#
+def clear_hints
+  UrlList.messages.each {|pointer, message| Weechat.hdata_update(Weechat.hdata_get('line_data'), pointer, { 'message' => message }) }
+  UrlList.clear
+end
+
+#
+# Dummy hook callback.
+#
 def hook_process_cb(data, command, rc, stdout, stderr)
   return Weechat::WEECHAT_RC_OK
 end
 
-def get_continue_count(buffer)
-  own_lines = Weechat.hdata_pointer(Weechat.hdata_get('buffer'), buffer, 'own_lines')
-
-  line_count = Weechat.hdata_integer(Weechat.hdata_get('lines'), own_lines, 'lines_count')
-  max_lines  = Weechat.hdata_integer(Weechat.hdata_get('window'), Weechat.current_window, 'win_chat_height')
-  line_count - max_lines
-end
+#----------------------------------
+# Wrapper of weechat hdata objects.
+#----------------------------------
 
 #
-# Check whether if strings like an url 'http://...' or 'https://...' is included in the buffer.
+# Wrapper of weechat hdata window.
 #
-def include_url?(buffer)
-  line = Buffer.new(buffer).own_lines.first_line
-
-  while true
-    return true if line.message =~ /(https?:\/\/[^ \(\)\r\n]*)/
-    break unless line = line.next
+class Window
+  class << self
+    def current
+      Window.new(Weechat.current_window)
+    end
   end
 
-  false
+  def initialize(pointer)
+    @pointer = pointer
+  end
+
+  def chat_height
+    Weechat.hdata_integer(Weechat.hdata_get('window'), @pointer, 'win_chat_height')
+  end
 end
 
+#
+# Wrapper of weechat hdata buffer.
+#
 class Buffer
   def initialize(pointer)
     @pointer = pointer
@@ -135,9 +143,18 @@ class Buffer
     own_lines_pointer = Weechat.hdata_pointer(Weechat.hdata_get('buffer'), @pointer, 'own_lines')
     Lines.new(own_lines_pointer)
   end
+
+  def has_url?
+    !own_lines.find(&:has_url?).nil?
+  end
 end
 
+#
+# Wrapper of weechat hdata lines.
+#
 class Lines
+  include Enumerable
+
   def initialize(pointer)
     @pointer = pointer
   end
@@ -146,16 +163,38 @@ class Lines
     first_line_pointer = Weechat.hdata_pointer(Weechat.hdata_get('lines'), @pointer, 'first_line')
     Line.new(first_line_pointer)
   end
+
+  def count
+    Weechat.hdata_integer(Weechat.hdata_get('lines'), @pointer, 'lines_count')
+  end
+
+  def each
+    line = first_line
+
+    while true
+      yield(line)
+      break unless line = line.next
+    end
+  end
 end
 
+#
+# Wrapper of weechat hdata line and line_data.
+#
 class Line
+  attr_reader :data_pointer
+
   def initialize(pointer)
     @pointer = pointer
     @data_pointer = Weechat.hdata_pointer(Weechat.hdata_get('line'), @pointer, 'data')
   end
 
   def message
-    Weechat.hdata_string(Weechat.hdata_get('line_data'), @data_pointer, 'message').to_s
+    @message ||= Weechat.hdata_string(Weechat.hdata_get('line_data'), @data_pointer, 'message').to_s
+  end
+
+  def message=(new_message)
+    Weechat.hdata_update(Weechat.hdata_get('line_data'), @data_pointer, { 'message' => new_message })
   end
 
   def next
@@ -166,12 +205,12 @@ class Line
   def displayed?
     Weechat.hdata_char(Weechat.hdata_get('line_data'), @data_pointer, 'displayed').to_s == '1'
   end
-end
 
-#
-# Clear hints.
-#
-def clear_hints
-  UrlList.messages.each {|pointer, message| Weechat.hdata_update(Weechat.hdata_get('line_data'), pointer, { 'message' => message }) }
-  UrlList.clear
+  def has_url?
+    !/https?:\/\/[^ \(\)\r\n]*/.match(message).nil?
+  end
+
+  def urls
+    message.scan(/https?:\/\/[^ \(\)\r\n]*/).uniq
+  end
 end
